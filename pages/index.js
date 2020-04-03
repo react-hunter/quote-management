ec40f1c387716a4c41107639897584352ab6857e
Jin Yong <yong@halfhelix.com>
2020-04-03 10:41:55 -0400

    fix a problem in selecting and add tabs

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
-import { EmptyState, Layout, Page } from '@shopify/polaris';
-import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+import { EmptyState, Layout, Page, Card, Tabs } from '@shopify/polaris';
+import { Redirect } from '@shopify/app-bridge/actions';
+import { ResourcePicker, TitleBar, Context } from '@shopify/app-bridge-react';
 import store from 'store-js';
 import ResourceListWithDraftOrders from '../components/ResourceDraftorderList';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 class Index extends React.Component {
-  state = { open: false };
-  app = this.context;
+  state = { 
+    open: false,
+    selectedTab: 0,
+  };
+
+  handleTabChange = (selectedTab) => {
+    this.setState({
+      selectedTab
+    });
+  }
+
   render() {
     const app = this.context;
     const gotoCreate = () => {
@@ -18,27 +28,56 @@ class Index extends React.Component {
       );
     };
 
+
+    const tabs = [
+      {
+        id: 'requested-by-customer',
+        content: 'Requested By Customer',
+        accessibilityLabel: 'Requested By Customer',
+        panelID: 'requested-by-customer-content',
+        filter: 'all',
+      },
+      {
+        id: 'approved',
+        content: 'Approved',
+        accessibilityLabel: 'Approved',
+        panelID: 'approved-content',
+        filter: 'approved',
+      },
+      {
+        id: 'active',
+        content: 'Active',
+        accessibilityLabel: 'Active',
+        panelID: 'active-content',
+        filter: 'active',
+      },
+      {
+        id: 'inactive',
+        content: 'Inactive',
+        accessibilityLabel: 'Inactive',
+        panelID: 'inactive-content',
+        filter: 'inactive',
+      }
+    ]
+
     return (
       <Page
         title="Quote"
         primaryAction={{
           content: 'Create Quote',
-          onAction: () => this.gotoCreate(),
+          onAction: () => gotoCreate(),
         }}
       >
-        <ResourceListWithDraftOrders />
+        <Card>
+          <Tabs tabs={tabs} selected={this.state.selectedTab} onSelect={this.handleTabChange}>
+            <Card.Section>
+              <ResourceListWithDraftOrders classify={tabs[this.state.selectedTab].filter} />
+            </Card.Section>
+          </Tabs>
+        </Card>
       </Page>
     );
   }
-  
-  gotoCreate = () => {
-    const redirect = Redirect.create(app);
-    redirect.dispatch(
-      Redirect.Action.APP,
-      '/create-quote',
-    );
-  };
-
 }
 
 export default Index;
